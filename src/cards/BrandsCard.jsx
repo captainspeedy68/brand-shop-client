282985aafac8cfe96e43871ddca20df48ef8e319
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-04-19 13:20:46 +0600

    brand cards made

---
+++ src/cards/BrandsCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const BrandsCard = ({ brand }) => {
+    const { image, brand: brandName } = brand;
+    // console.log(brandName);
+    // console.log(brand)
+    const handleBrand = () => {
+        console.log("clicked")
+    }
+    return (
+        <NavLink to={`/brands/${brandName}`}>
+            <div>
+                <div className="w-72 h-72 rounded-none shadow-md border border-red-100">
+                    <figure><img className='h-48 w-full bg-red-300' src={image} alt="car!" /></figure>
+                    <hr />
+                    <div className="card-body">
+                        <h2 className="card-title justify-center text-red-500">{brandName}</h2>
+                    </div>
+                </div>
+            </div>
+        </NavLink>
+    );
+};
+
+export default BrandsCard;
