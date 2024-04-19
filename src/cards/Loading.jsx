788fe8d7c60d15126857117e3b11841d9cee7958
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-04-19 13:17:40 +0600

    loading card

---
+++ src/cards/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='h-full w-full flex justify-center text-center my-auto items-center align-middle'>
+            <span className="loading loading-spinner loading-lg"></span>
+        </div>
+    );
+};
+
+export default Loading;
