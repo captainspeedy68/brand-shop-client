bd5d88c4b2958433b98b296f73627d9e567019ed
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-04-21 00:22:32 +0600

    toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import App from './App.jsx'
 import './index.css'
 import AuthProvider from './providers/AuthProvider.jsx'
 import router from './routes/router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
+  <AuthProvider>
+    <React.StrictMode>
       <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>,
+      <ToastContainer autoClose={1000}></ToastContainer>
+    </React.StrictMode>
+  </AuthProvider>,
 )
