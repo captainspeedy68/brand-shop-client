b379e66980155e6704f1363e6da7af2d355bbe4f
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-04-21 00:22:56 +0600

    loading element

--- src/cards/Loading.jsx
+++ src/cards/Loading.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className='h-full w-full flex justify-center text-center my-auto items-center align-middle'>
-            <span className="loading loading-spinner loading-lg"></span>
+        <div className='h-full max-h-screen w-full my-auto'>
+
+            <div className='h-full w-full flex justify-center text-center my-auto items-center align-middle'>
+                <span className="loading loading-spinner loading-lg h-full my-auto py-auto"></span>
+            </div>
         </div>
     );
 };
